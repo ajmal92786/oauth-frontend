7b882a4ffbbdc30b037ca717500bed223e320083
Mohd Ajmal Raza <mohdajmalraza92786@gmail.com>
2025-06-14 17:41:12 +0530

    fix: enable credentials in Axios to allow cookie-based auth with backend

--- src/lib/axios.lib.js
+++ src/lib/axios.lib.js
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const authServerAxios = axios.create({
   baseURL: `${import.meta.env.VITE_SERVER_BASE_URL}`,
   withCredentials: true,
+  credentials: "include",
 });
 
 export const githubApiAxios = axios.create({
